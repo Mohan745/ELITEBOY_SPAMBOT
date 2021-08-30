94d44be722889bb3953e1652195c2238b4f4cbad
HARRY <87848754+ITX-ALONE-HARRY@users.noreply.github.com>
2021-08-30 22:32:46 +0530

    Update __main__.py

--- SpamBots/__main__.py
+++ SpamBots/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("Enjoy! Do visit @Zaid_Updates")
+print("Bot start ho gays bsdk ja subki gand mar 🥵")
 
 if __name__ == "__main__":
     UstaD.run_until_disconnected()
